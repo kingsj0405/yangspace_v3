1efc9a9d5526566d1bda76f9636bce4c2e677de5
kingsj0405 <kingsj0405@naver.com>
2018-01-21 23:34:45 +0100

    Fix error of `SECRET_KEY` for django
    It's recommended to turn off `Optimize imports` function of `PyCharm`.

--- YangSpace/settings/base.py
+++ YangSpace/settings/base.py
@@ -14,6 +14,8 @@ import os
 
 from django.utils.translation import ugettext_lazy as _
 
+from .secret import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
