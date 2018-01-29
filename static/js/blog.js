1ee46b1b8fa1b6841bc642e560b3461a1dd605a6
kingsj0405 <kingsj0405@naver.com>
2018-01-29 22:02:29 +0100

    Implement simple editor using 'showdown'
    Not capsulized.
    Add `div` to textarea and swap to preview.

---
+++ static/js/blog.js
@@ -0,0 +1,48 @@
+// Global Variable
+var editor_id = 'content',
+    viewer_id = 'content-viewer';
+var $editor, $viewer;
+
+function init_editor() {
+    // Initialize variables
+    $editor = $('#' + editor_id);
+    $('#content').after("<div id='" + viewer_id + "' tabindex='1'></div>");
+    $viewer = $('#' + viewer_id);
+    $viewer.hide();
+
+    // Initialize content
+    showdown.setFlavor('github');
+
+    // Add event function
+    $editor.keydown(render_viewer);
+    $viewer.keydown(render_editor);
+    $viewer.focusout(function () {
+        $(this).focus();
+    })
+}
+
+function render_viewer(e) {
+    if (e.ctrlKey && e.keyCode === 13) {
+        // Ctrl-Enter
+        var text = $editor.val(),
+            converter = new showdown.Converter(),
+            html = converter.makeHtml(text);
+        $viewer.html(html);
+        $editor.hide();
+        $viewer.show();
+        $viewer.focus();
+    }
+}
+
+function render_editor(e) {
+    if (e.keyCode === 27) {
+        // ESC
+        $viewer.hide();
+        $editor.show();
+        $editor.focus();
+    }
+}
+
+$(document).ready(function () {
+    init_editor();
+});
