de501f03e802633402bc25cd10d0db690937b7e7
kingsj0405 <kingsj0405@naver.com>
2018-02-04 07:01:59 +0100

    Backup 스크립트가 Bash를 사용해서 올바로 동작하도록 수정
    batch 스크립트와 헷갈려서 제대로 동작하지 않는 스크립트여서 수정했다.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-pushd ../
+cur_dir=$(pwd)
+src_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)"
+
+cd $src_dir
 
 docker-compose exec web python manage.py dumpdata > data/dump_`date +%d-%m-%Y"_"%H_%M_%S`.json
 
-popd
+cd $cur_dir
