fd5650dee417951b24743ce3edbeed387562e1a4
kingsj0405 <kingsj0405@naver.com>
2018-02-04 16:03:14 +0100

    백업파일이 최대 7개까지만 존재할 수 있도록 'backup.sh'를 수정
    지나치게 많아지면 서버 디스크에 과부하가 올 수 있다.
    이 이상 보관하기 위해서는 백업파일 보관을 다른 곳에 하는 방식으로 변경이 필요하다.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,4 +7,9 @@ cd $src_dir
 
 docker-compose exec web python manage.py dumpdata > data/dump_`date +%d-%m-%Y"_"%H_%M_%S`.json
 
+while [ $(ls data -1 | grep .json | wc -l) -gt 7 ]
+do
+  rm data/$(ls data -1 | grep -m1 .json)
+done
+
 cd $cur_dir
