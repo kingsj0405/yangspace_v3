afcbc160efce19ec36a75f33916668b68c626fc8
kingsj0405 <kingsj0405@naver.com>
2018-03-13 17:43:09 +0100

    SimpleMDE를 편집기로 이용
    View를 ShowDown을 이용하기때문에 동기화 문제가 발생할 수 있긴하지만 그런 건 작성자가 조심.

--- README.md
+++ README.md
@@ -79,6 +79,7 @@ Followings are downloaded in `static/lib`.
 - [Bootstrap 3.3.7](https://getbootstrap.com/docs/3.3/)
 - [Font Awesome](http://fontawesome.io/) 4.7.0
 - [jQuery](https://jquery.com/) 3.2.1
+- [SimpleMDE](https://github.com/sparksuite/simplemde-markdown-editor) 1.11.2
 - [showdown](https://github.com/showdownjs/showdown) 1.8.4
 
 #### Django Pakcage
--- static/js/mdeditor.js
+++ static/js/mdeditor.js
@@ -1,51 +1,7 @@
-// Markdown Editor
-var editor_id = 'content',
-    viewer_id = 'content-viewer';
-var $editor, $viewer;
+var editor_id = 'content';
 
 function init_editor() {
-    // Initialize variables
-    $editor = $('#' + editor_id);
-    $('#content').after("<div id='" + viewer_id + "' tabindex='1'></div>");
-    $viewer = $('#' + viewer_id);
-    $viewer.hide();
-
-    // Initialize content
-    showdown.setFlavor('github');
-
-    // Add event function
-    $editor.keydown(render_viewer);
-    $viewer.keydown(render_editor);
-    $viewer.focusout(function () {
-        $(this).focus();
-    })
-}
-
-function render_viewer(e) {
-    if (e.ctrlKey && e.keyCode === 13) {
-        // Ctrl-Enter
-        var text = $editor.val(),
-            converter = new showdown.Converter(),
-            html = converter.makeHtml(text);
-        $viewer.html(html);
-
-        var scroll_top = $editor.scrollTop();
-        $editor.hide();
-        $viewer.show();
-        $viewer.focus();
-        $viewer.scrollTop(scroll_top);
-    }
-}
-
-function render_editor(e) {
-    if (e.keyCode === 27) {
-        // ESC
-        var scroll_top = $viewer.scrollTop();
-        $viewer.hide();
-        $editor.show();
-        $editor.focus();
-        $editor.scrollTop(scroll_top);
-    }
+    var simplemde = new SimpleMDE({ element: $('#' + editor_id)[0] });
 }
 
 $(document).ready(function () {
